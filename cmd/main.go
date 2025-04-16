f2a6009f2005a5db2571e0c47147e6130b7d07cf
gusslik <grigory.bocharsky@gmail.com>
2025-04-16 16:24:41 +0100

    set up routers, handlers and services

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"todo-web-app-go/internal/router"
 )
 
 func main() {
-	http.ListenAndServe("localhost:8000", router.Setup())
+	fmt.Println("Server is starting...")
+	log.Fatal(http.ListenAndServe("localhost:8000", router.Setup()))
 }
